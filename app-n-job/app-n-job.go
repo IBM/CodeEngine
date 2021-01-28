4da183df3ea563d950f9dcfb02d12db872cd98d8
Doug Davis <dug@us.ibm.com>
2021-01-28 20:56:46 +0000

    add logging
    Signed-off-by: Doug Davis <dug@us.ibm.com>

--- app-n-job/app-n-job.go
+++ app-n-job/app-n-job.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
@@ -10,14 +11,15 @@ func main() {
 	jobIndex := os.Getenv("JOB_INDEX")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("log: app-n-job got an incoming request\n")
 		fmt.Fprintf(w, "Hello from app-n-job! I'm a web app!\n")
 	})
 
 	if jobIndex == "" {
-		fmt.Printf("Listening on port 8080\n")
+		log.Printf("Listening on port 8080\n")
 		http.ListenAndServe(":8080", nil)
 	} else {
-		fmt.Printf("Hello from app-n-job! I'm a batch job! Index: %s\n",
+		log.Printf("Hello from app-n-job! I'm a batch job! Index: %s\n",
 			jobIndex)
 	}
 }
