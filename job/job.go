4921d605ea1a3d47bcfb3a145a7a1f9d89553e7a
ASEIDEL77 <Andreas.Seidel@de.ibm.com>
2023-11-24 12:21:17 +0100

    Update job/job.go
    Co-authored-by: Enrico Regge <36001299+reggeenr@users.noreply.github.com>

--- job/job.go
+++ job/job.go
@@ -6,5 +6,5 @@ import (
 )
 
 func main() {
-	fmt.Printf("Hi from a batch job! My index is: %s, custom JOB_ARRAY_SIZE value is: %s\n", os.Getenv("JOB_INDEX"), os.Getenv("JOB_ARRAY_SIZE"))
+	fmt.Printf("Hi from a batch job! My index within the array of %s instance(s) is %s\n", os.Getenv("JOB_ARRAY_SIZE"), os.Getenv("JOB_INDEX"))
 }
