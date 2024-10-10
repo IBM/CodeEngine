18beb7924f633a6d887dea71e3e97e1afb6e9aaf
Enrico Regge <reggeenr@de.ibm.com>
2024-10-10 22:33:14 +0200

    Converted the hello sample to use distroless nodejs

--- hello/server.js
+++ hello/server.js
@@ -1,10 +1,28 @@
-const http = require('http');
+const http = require("http");
 
-http.createServer(function (request, response) {
-   target = process.env.TARGET ? process.env.TARGET : 'World' ;
-   msg = process.env.MSG ? process.env.MSG : 'Hello ' + target + '\n';
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end(msg);
-}).listen(8080);
+http
+  .createServer(function (request, response) {
+    //
+    // debug endpoint, which prints all incoming headers and environment variables
+    if (request.url == "/debug") {
+      const respData = {
+        headers: request.headers,
+        url: request.url,
+        method: request.method,
+        env: process.env,
+      };
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.end(JSON.stringify(respData));
+      return;
+    }
 
-console.log('Server running at http://0.0.0.0:8080/');
+    //
+    // default http endpoint, which prints a simple hello world
+    target = process.env.TARGET ? process.env.TARGET : "World";
+    msg = process.env.MSG ? process.env.MSG : "Hello " + target + "\n";
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end(msg);
+  })
+  .listen(8080);
+
+console.log("Server running at http://0.0.0.0:8080/");
