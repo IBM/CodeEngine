3557b20f430245b31d0bcc8255860fd199927067
Luke Roy <luke.roy@ibm.com>
2025-04-29 16:43:08 +0200

    touch ups
    Signed-off-by: Luke Roy <luke.roy@ibm.com>

--- fotobox/cos-setup/main.tf
+++ fotobox/cos-setup/main.tf
@@ -1,6 +1,6 @@
 # create a cos instance
 resource "ibm_resource_instance" "cos_instance" {
-  name = "cos-instance"
+  name = "cos-fotobox-instance"
   resource_group_id = var.resource_group
   plan = "standard"
   service = "cloud-object-storage"
@@ -45,16 +45,16 @@ resource "ibm_code_engine_secret" "fotobox-secret" {
   }
 }
 
-resource "ibm_code_engine_secret" "registry_secret" {
-  project_id = ibm_code_engine_project.ce-fotobox-project.id
-  name       = "container-registry-secret"
-  format = "registry"
-  data = {
-    "username" = "iamapikey" # Use 'iamapikey' as username for IBM Cloud
-    "password" = var.icr_secret
-    "server"   = "us.icr.io" # Change if using a different registry
-  }
-}
+#resource "ibm_code_engine_secret" "registry_secret" {
+#  project_id = ibm_code_engine_project.ce-fotobox-project.id
+#  name       = "container-registry-secret"
+#  format = "registry"
+#  data = {
+#    "username" = "iamapikey" # Use 'iamapikey' as username for IBM Cloud
+#    "password" = var.icr_secret
+#    "server"   = "us.icr.io" # Change if using a different registry
+#  }
+#}
 
 # create config map in project
 
@@ -63,9 +63,9 @@ resource "ibm_code_engine_config_map" "fotobox-config" {
   project_id = ibm_code_engine_project.ce-fotobox-project.id
   data = {
     "bucket" = ibm_cos_bucket.cos_bucket.bucket_name
-    "endpointURL" = ibm_cos_bucket.cos_bucket.s3_endpoint_private
+    "endpointURL" = ibm_cos_bucket.cos_bucket.s3_endpoint_public
     "imageprefix" = "my-event-"
-    "password" = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8"
+    "passwords" = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8"
     "region" = ibm_cos_bucket.cos_bucket.region_location
     "resource_instance_id" = local.resource_credentials.resource_instance_id
   }
