12386216a04aa108c89113cf7750d903a07f3732
Shailza Thakur <shailza.thakur@ibm.com>
2025-07-01 18:16:32 +0530

    Remove option for service-credentials. Set 3 variables to default properties
    Signed-off-by: Hamza Bharmal <hamza@mac.in.ibm.com>

--- code-engine-cos2cos/main.go
+++ code-engine-cos2cos/main.go
@@ -5,7 +5,6 @@ import (
 	"os"
 
 	"github.com/IBM/ibm-cos-sdk-go/service/s3"
-	"github.com/joho/godotenv"
 	"ibm.com/codeengine/cos2cos/bucketOperations"
 	"ibm.com/codeengine/cos2cos/process"
 )
@@ -16,74 +15,34 @@ import (
 func main() {
 	fmt.Println("Starting application with Job Index:", os.Getenv("JOB_INDEX"))
 
-	// Default Values for running the program
-	// Use service credentials and
-	var isInCodeEngine bool = true
-	var isUsingTrustedProfile bool = true
+	os.Setenv("IBM_COS_CRTokenFilePath_PRIMARY", "/var/run/secrets/codeengine.cloud.ibm.com/compute-resource-token/token")
+	os.Setenv("IBM_COS_CRTokenFilePath_SECONDARY", "/var/run/secrets/codeengine.cloud.ibm.com/compute-resource-token/token")
+	os.Setenv("BUCKET_TIMESTAMP_FILENAME", "last_modified_time.json")
 
-	// First argument states if the program is running in code engine env. (Default: Yes)
-	if len(os.Args) > 1 {
-		isInCodeEngine = os.Args[1] == "true"
-		if !isInCodeEngine {
-			os.Setenv("JOB_ARRAY_SIZE", "1")
-			os.Setenv("JOB_INDEX", "0")
-		}
-	}
+	fmt.Println("Any errors while processing objects:", processingMain())
 
-	// Second argument states if the program uses trustedProfile (Default: Yes)
-	if len(os.Args) > 2 {
-		isUsingTrustedProfile = os.Args[2] == "true"
-	}
-
-	if !isInCodeEngine {
-		err := godotenv.Load()
-		if err != nil {
-			fmt.Println("Error loading env file")
-			return
-		}
-	}
-
-	fmt.Println("Any errors while processing objects:", processingMain(isUsingTrustedProfile))
-
-	fmt.Println("Clsoing application")
+	fmt.Println("Closing application")
 }
 
 // Function will initiate the buckets, sessions and trigger the StartProcessing function for actual processing
 // accepts a boolean argument which decides how to create COS-Client (Using TrustedProfile or Service Credentials)
-func processingMain(useTrustedProfile bool) error {
-	// fmt.Println("hehe")
-
+func processingMain() error {
 	var cosClient_PRIMARY, cosClient_SECONDARY *s3.S3
 
-	if !useTrustedProfile {
-		// Create a cosClient using Service Credentials
-		cosClient_PRIMARY = bucketOperations.NewCosClient(os.Getenv("IBM_COS_API_KEY_PRIMARY"),
-			os.Getenv("IBM_COS_RESOURCE_INSTANCE_ID_PRIMARY"),
-			os.Getenv("IBM_COS_ENDPOINT_PRIMARY"),
-			os.Getenv("IBM_COS_REGION_PRIMARY"),
-		)
+	// Create a cosClient using Trusted Profile
+	cosClient_PRIMARY = bucketOperations.NewCosClientTrustedProfile(os.Getenv("IBM_COS_TRUSTED_PROFILE_ID_PRIMARY"),
+		os.Getenv("IBM_COS_CRTokenFilePath_PRIMARY"),
+		os.Getenv("IBM_COS_RESOURCE_INSTANCE_ID_PRIMARY"),
+		os.Getenv("IBM_COS_ENDPOINT_PRIMARY"),
+		os.Getenv("IBM_COS_REGION_PRIMARY"),
+	)
 
-		cosClient_SECONDARY = bucketOperations.NewCosClient(os.Getenv("IBM_COS_API_KEY_SECONDARY"),
-			os.Getenv("IBM_COS_RESOURCE_INSTANCE_ID_SECONDARY"),
-			os.Getenv("IBM_COS_ENDPOINT_SECONDARY"),
-			os.Getenv("IBM_COS_REGION_SECONDARY"),
-		)
-	} else {
-		// Create a cosClient using Trusted Profile
-		cosClient_PRIMARY = bucketOperations.NewCosClientTrustedProfile(os.Getenv("IBM_COS_TRUSTED_PROFILE_ID_PRIMARY"),
-			os.Getenv("IBM_COS_CRTokenFilePath_PRIMARY"),
-			os.Getenv("IBM_COS_RESOURCE_INSTANCE_ID_PRIMARY"),
-			os.Getenv("IBM_COS_ENDPOINT_PRIMARY"),
-			os.Getenv("IBM_COS_REGION_PRIMARY"),
-		)
-
-		cosClient_SECONDARY = bucketOperations.NewCosClientTrustedProfile(os.Getenv("IBM_COS_TRUSTED_PROFILE_ID_SECONDARY"),
-			os.Getenv("IBM_COS_CRTokenFilePath_SECONDARY"),
-			os.Getenv("IBM_COS_RESOURCE_INSTANCE_ID_SECONDARY"),
-			os.Getenv("IBM_COS_ENDPOINT_SECONDARY"),
-			os.Getenv("IBM_COS_REGION_SECONDARY"),
-		)
-	}
+	cosClient_SECONDARY = bucketOperations.NewCosClientTrustedProfile(os.Getenv("IBM_COS_TRUSTED_PROFILE_ID_SECONDARY"),
+		os.Getenv("IBM_COS_CRTokenFilePath_SECONDARY"),
+		os.Getenv("IBM_COS_RESOURCE_INSTANCE_ID_SECONDARY"),
+		os.Getenv("IBM_COS_ENDPOINT_SECONDARY"),
+		os.Getenv("IBM_COS_REGION_SECONDARY"),
+	)
 
 	// Creating Primary bucket instance
 	primary_bucket := bucketOperations.NewBucket(
@@ -96,15 +55,14 @@ func processingMain(useTrustedProfile bool) error {
 		os.Getenv("SECONDARY_COS_BUCKET_NAME"),
 		cosClient_SECONDARY,
 	)
-	// fmt.Println("hehe")
 	// Initiate the process from primary bucket to secondary bucket
 	// err := process.StartProcessing(primary_bucket, secondary_bucket)
 	err := process.StartProcessingPagination(primary_bucket, secondary_bucket)
 	if err != nil {
-		return fmt.Errorf("\nBackup failed: %v", err)
+		return fmt.Errorf("\nProcessing failed: %v", err)
 	}
 
-	fmt.Println("\nBackup completed successfully.")
+	fmt.Println("\nProcessing completed successfully.")
 
 	return nil
 }
