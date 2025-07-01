009f50e90468eb112a2747b0ed130c86094e5f0b
Jeremias Werner <JEREWERN@de.ibm.com>
2025-07-01 18:07:18 +0200

    initial version of the experimental fleet tutorial

---
+++ experimental/serverless-fleets/tutorials/docling/minimal.py
@@ -0,0 +1,7 @@
+from docling.document_converter import DocumentConverter
+
+source = "https://arxiv.org/pdf/2408.09869"  # document per local path or URL
+converter = DocumentConverter()
+result = converter.convert(source)
+print(result.document.export_to_markdown())
+# output: ## Docling Technical Report [...]"
