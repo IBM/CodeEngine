0eb88fe63363b4d0fe2dfabc71e837de9ba30106
Hamza <Hamza@ibm.com>
2025-07-02 18:06:22 +0530

    Remove extra variables to default values
    Signed-off-by: Hamza Bharmal <hamza@mac.in.ibm.com>

--- code-engine-cos2cos/data.sh
+++ code-engine-cos2cos/data.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # Regions
-COS_REGION_PRIMARY=au-syd
+COS_REGION_PRIMARY=eu-es
 COS_REGION_SECONDARY=eu-es
 PROJECT_REGION=eu-es
 PROJECT_NAME="Cos2Cos"
 
 # Resource groups
 PROJECT_RESOURCE_GROUP=resource-group
-COS_RESOURCE_GROUP_PRIMARY=resource-group
-COS_RESOURCE_GROUP_SECONDARY=resource-group
+COS_RESOURCE_GROUP_PRIMARY=${PROJECT_RESOURCE_GROUP}
+COS_RESOURCE_GROUP_SECONDARY=${PROJECT_RESOURCE_GROUP}
 
 #Creating a COS Instance
 COS_INSTANCE_NAME_PRIMARY=cos-instance-one
@@ -20,7 +20,7 @@ COS_BUCKET_NAME_PRIMARY=cos-bucket-one
 COS_BUCKET_NAME_SECONDARY=cos-bucket-two
 
 # Endpoints
-COS_ENDPOINT_PRIMARY=s3.au-syd.cloud-object-storage.appdomain.cloud
+COS_ENDPOINT_PRIMARY=s3.eu-es.cloud-object-storage.appdomain.cloud
 COS_ENDPOINT_SECONDARY=s3.eu-es.cloud-object-storage.appdomain.cloud
 
 # Trusted Profile
@@ -32,6 +32,3 @@ JOB_NAME=cos2cos-job
 # Define registry credentials
 BASE_SECRET=ce-secret
 AUTH_SECRET=auth-secret
-
-# Array Size of Job run Instance
-ARRAY_SIZE=13
