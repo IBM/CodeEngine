4d8601809c21b906332428d902b04b5d26c269df
Hamza <Hamza@ibm.com>
2025-07-02 18:07:59 +0530

    Update to use default values, Support single-trusted-profile
    Signed-off-by: Hamza Bharmal <hamza@mac.in.ibm.com>

--- code-engine-cos2cos/main.go
+++ code-engine-cos2cos/main.go
@@ -12,11 +12,6 @@ import (
 func main() {
 	fmt.Println("Starting application with Job Index:", os.Getenv("JOB_INDEX"))
 
-	// Setting default values
-	os.Setenv("IBM_COS_CRTokenFilePath_PRIMARY", "/var/run/secrets/codeengine.cloud.ibm.com/compute-resource-token/token")
-	os.Setenv("IBM_COS_CRTokenFilePath_SECONDARY", "/var/run/secrets/codeengine.cloud.ibm.com/compute-resource-token/token")
-	os.Setenv("BUCKET_TIMESTAMP_FILENAME", "last_modified_time.json")
-
 	fmt.Println("Any errors while processing objects:", processingMain())
 
 	fmt.Println("Closing application")
@@ -27,16 +22,19 @@ func main() {
 func processingMain() error {
 	var cosClient_PRIMARY, cosClient_SECONDARY *s3.S3
 
+	// CR Token_FilePath (stays default)
+	crTokenFilePath := "/var/run/secrets/codeengine.cloud.ibm.com/compute-resource-token/token"
+
 	// Create a cosClient using Trusted Profile
-	cosClient_PRIMARY = bucketOperations.NewCosClientTrustedProfile(os.Getenv("IBM_COS_TRUSTED_PROFILE_ID_PRIMARY"),
-		os.Getenv("IBM_COS_CRTokenFilePath_PRIMARY"),
+	cosClient_PRIMARY = bucketOperations.NewCosClientTrustedProfile(os.Getenv("IBM_COS_TRUSTED_PROFILE_ID"),
+		crTokenFilePath,
 		os.Getenv("IBM_COS_RESOURCE_INSTANCE_ID_PRIMARY"),
 		os.Getenv("IBM_COS_ENDPOINT_PRIMARY"),
 		os.Getenv("IBM_COS_REGION_PRIMARY"),
 	)
 
-	cosClient_SECONDARY = bucketOperations.NewCosClientTrustedProfile(os.Getenv("IBM_COS_TRUSTED_PROFILE_ID_SECONDARY"),
-		os.Getenv("IBM_COS_CRTokenFilePath_SECONDARY"),
+	cosClient_SECONDARY = bucketOperations.NewCosClientTrustedProfile(os.Getenv("IBM_COS_TRUSTED_PROFILE_ID"),
+		crTokenFilePath,
 		os.Getenv("IBM_COS_RESOURCE_INSTANCE_ID_SECONDARY"),
 		os.Getenv("IBM_COS_ENDPOINT_SECONDARY"),
 		os.Getenv("IBM_COS_REGION_SECONDARY"),
@@ -44,17 +42,16 @@ func processingMain() error {
 
 	// Creating Primary bucket instance
 	primary_bucket := bucketOperations.NewBucket(
-		os.Getenv("PRIMARY_COS_BUCKET_NAME"),
+		os.Getenv("COS_BUCKET_NAME_PRIMARY"),
 		cosClient_PRIMARY,
 	)
 
 	// Creating secondary bucket instance
 	secondary_bucket := bucketOperations.NewBucket(
-		os.Getenv("SECONDARY_COS_BUCKET_NAME"),
+		os.Getenv("COS_BUCKET_NAME_SECONDARY"),
 		cosClient_SECONDARY,
 	)
 	// Initiate the process from primary bucket to secondary bucket
-	// err := process.StartProcessing(primary_bucket, secondary_bucket)
 	err := process.StartProcessingPagination(primary_bucket, secondary_bucket)
 	if err != nil {
 		return fmt.Errorf("\nProcessing failed: %v", err)
