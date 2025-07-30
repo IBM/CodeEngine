b94f22710996df44e570aa0e7cd0273e6fa54c2c
Jeremias Werner <JEREWERN@de.ibm.com>
2025-07-30 11:55:49 +0200

    rebase docling example to use official image and tasks-from-file

--- experimental/serverless-fleets/tutorials/docling/minimal.py
+++
@@ -1,7 +0,0 @@
-from docling.document_converter import DocumentConverter
-
-source = "https://arxiv.org/pdf/2408.09869"  # document per local path or URL
-converter = DocumentConverter()
-result = converter.convert(source)
-print(result.document.export_to_markdown())
-# output: ## Docling Technical Report [...]"
