a1d2193e343a1986cd7e35fcca4b90bdcf7d4dfe
Eric Liu <ericyl.us@gmail.com>
2025-09-13 10:51:08 -0700

    Add `optimizeImports` to svelte.config.js

--- fotobox/frontend-app/svelte.config.js
+++ fotobox/frontend-app/svelte.config.js
@@ -1,7 +1,12 @@
 import adapter from '@sveltejs/adapter-node';
+import { optimizeImports } from 'carbon-preprocess-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: [
+		// Optimize Carbon imports for faster development and build times.
+		optimizeImports()
+	],
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
