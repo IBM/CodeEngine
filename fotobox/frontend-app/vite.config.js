95fb9e6a22f5a8d683d705e4062a48f335ddaad6
Eric Liu <ericyl.us@gmail.com>
2025-09-13 10:52:03 -0700

    Add `optimizeCss` to vite.config.js

--- fotobox/frontend-app/vite.config.js
+++ fotobox/frontend-app/vite.config.js
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { optimizeCss } from 'carbon-preprocess-svelte';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+
+		// Optimize CSS from `carbon-components-svelte` when building for production.
+		optimizeCss()
+	]
 });
